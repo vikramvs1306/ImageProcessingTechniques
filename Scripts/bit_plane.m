e27d9852d451eac224c5200d124b329f895539ff
vikramvs1306 <42532717+vikramvs1306@users.noreply.github.com>
2023-12-25 04:46:53 -0500

    Update bit_plane.m

--- Scripts/bit_plane.m
+++ Scripts/bit_plane.m
@@ -1,20 +1,22 @@
-function [image] = bit_plane(image, bit_plane_number)
-image = double(image);
-% Extract the specified bit plane
-bit_plane_image = bitget(image, bit_plane_number);
-% Display the bit plane
-figure;
-imshow(bit_plane_image);
+function bit_plane_image = bit_plane(image, bit_plane_number)
+    image = double(image);
+    % Extract the specified bit plane
+    bit_plane_image = bitget(image, bit_plane_number);
+    % Display the bit plane
+    figure;
+    imshow(logical(bit_plane_image)); % Convert to logical for proper display
 end
-clc
-close all
+
+% Script to call the function
+clc;
+close all;
 peppers = imread('peppers.tif');
 baboon = imread('baboon.tif');
-%barbara = imread('Barbara.bmp');
+
 bit_plane_number = 4;
-peppers = bit_plane(peppers, bit_plane_number);
-baboon = bit_plane(baboon, bit_plane_number);
-%barbara = bit_plane(barbara, bit_plane_number);
+bit_plane(peppers, bit_plane_number);
+bit_plane(baboon, bit_plane_number);
+
 
 %"The highest bitplane for Peppers is bitplane 3 or 4 that no longer resembles
 %image contect instead it appears noise. And for baboon it is bitplane 4 or 5.
